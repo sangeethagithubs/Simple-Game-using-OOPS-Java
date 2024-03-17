d92bd5fba8095c724fe74ff26c0546be9f93d958
H K Sangeetha <137398167+sangeethagithubs@users.noreply.github.com>
2024-03-17 19:32:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
+package mypack;
 
+public class Main {
+
+    public static void main(String[] args) {
+      pack1.Player1 player1 = new pack1.Player1("Bindu","sword",100);
+          System.out.println("player1 Details:\n");
+          System.out.println(player1.getName());
+          System.out.println(player1.getHealth());
+          System.out.println(player1.getWeapon());
+      
+      pack1.Player1 player1 = new pack1.Player1("Raghu","sword",100,true);
+          System.out.println("player2 Details:\n");
+          System.out.println(player2.getName());
+          System.out.println(player2.getHealth());
+          System.out.println(player2.getWeapon());
+
+      player2.damageByGun1();
+      player2.damageByGun2();
+      
+    }
+
+}
